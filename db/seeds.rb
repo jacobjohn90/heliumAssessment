e1e9b63d64cf04a44eae2799290de250fb1dc020
jacobjohn90 <jacobjohn90@gmail.com>
2018-10-15 15:12:46 -0400

    refactored seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ james=Employee.create(
     last_name:'Smith',
     gender:0,
     title:'CEO',
-    birth_date: '01/15/1975'
+    birth_date: '05/01/1975'
 )
 
 linda=Employee.create(
@@ -15,7 +15,7 @@ linda=Employee.create(
     last_name:'Franko',
     gender:1,
     title:'CFO',
-    birth_date: '02/25/1962'
+    birth_date: '25/02/1962'
 )
 
 catherine=Employee.create(
@@ -23,7 +23,7 @@ catherine=Employee.create(
     last_name:'Dodge',
     gender:1,
     title:'Accountant',
-    birth_date: '05/26/1985'
+    birth_date: '26/05/1985'
 )
 
 david=Employee.create(
@@ -31,7 +31,7 @@ david=Employee.create(
     last_name:'Kim',
     gender:0,
     title:'Programmer',
-    birth_date: '07/09/1988'
+    birth_date: '09/07/1988'
 )
 
 joseph=Employee.create(
@@ -39,33 +39,33 @@ joseph=Employee.create(
     last_name:'Norton',
     gender:0,
     title:'Designer',
-    birth_date: '03/24/1992'
+    birth_date: '24/03/1992'
 )
-
 
 james = linda.children.create(
     first_name: 'James',
     last_name: 'Franko',
     gender:0,
-    birth_date: '04/17/1982'
+    birth_date: '17/04/1982'
 )
 
 julie = linda.children.create(
     first_name: 'Julie',
     last_name: 'Franko',
     gender:1,
-    birth_date: '11/19/1996'
+    birth_date: '19/11/1996'
 )
 
 taylor = catherine.children.create(
     first_name: 'Taylor',
     last_name: 'Smith',
     gender:0,
-    birth_date: '07/09/1998'
+    birth_date: '09/07/1998'
 )
+
 benjamin = joseph.children.create(
     first_name: 'Benjamin',
     last_name: 'Smith',
     gender:0,
-    birth_date: '03/24/1992'
+    birth_date: '24/03/1992'
 )
